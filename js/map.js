1f2da84e78b02d593fd36f4f4e01bd1ec9b2e532
Jota Bustos <jose.bustos.diaz@melia.com>
2024-02-10 16:26:24 +0100

    feat: add images and styles

--- js/map.js
+++ js/map.js
@@ -1,22 +1,22 @@
 $(window).on("load", function(){
-		//Open street  Map
-		var coord = [40.738270, -74.008911]; // <--- coordinates here
+	//Open street  Map
+	var coord = [40.677662, -3.972982]; // <--- coordinates here , 
 
-		var map = L.map('map-canvas', { scrollWheelZoom:false}).setView(coord, 19);
+	var map = L.map('map-canvas', { scrollWheelZoom:false}).setView(coord, 19);
 
-		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		maxZoom: 22,
-		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-		}).addTo(map);
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 22,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
 
 
-		// custom icon
-		var customIcon = L.icon({
-		iconUrl: 'img/mapmarker.png',
-		iconSize:     [64, 64], // size of the icon
-		iconAnchor:   [32, 63] // point of the icon which will correspond to marker's location
-		});
+	// custom icon
+	var customIcon = L.icon({
+	iconUrl: 'img/hdls/Logo HS_Alternativo_sobre blanco_vertical limpio.png',
+	iconSize:     [64, 64], // size of the icon
+	iconAnchor:   [32, 63] // point of the icon which will correspond to marker's location
+	});
 
-		// marker object, pass custom icon as option, add to map         
-		var marker = L.marker(coord, {icon: customIcon}).addTo(map);
-		});
+	// marker object, pass custom icon as option, add to map         
+	var marker = L.marker(coord, {icon: customIcon}).addTo(map);
+	});
